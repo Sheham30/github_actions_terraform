882e5891610e7daafb59397a653924aa15a150e3
sheham30 <shehamhassan@gmail.com>
2023-05-01 14:20:01 +0500

    resources added

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "mybucket" {
-  bucket = "microsystemsitterraform01052023"
+  bucket = "microsystemsitterraform01052024"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+
+resource "aws_instance" "this" {
+  ami                     = "ami-007855ac798b5175e"
+  instance_type           = "t2.micro"
+
+    tags = {
+    Name        = "MyInstance"
+  } 
 }
